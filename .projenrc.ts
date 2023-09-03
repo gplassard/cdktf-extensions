6224ccd10347c8214852afb69e73dd05bebd3f93
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2023-09-04 00:00:13 +0200

    feat(dd-tags): add dd tags object (#1)
    * feat(dd-tags): add dd tags object
    
    * chore: self mutation
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>

--- .projenrc.ts
+++ .projenrc.ts
@@ -3,7 +3,10 @@ import { TypescriptLibraryProject } from '@gplassard/projen-extensions';
 
 // opinionated wrapper around projen TypeScriptProject for libraries
 const project = new TypescriptLibraryProject({
-    name: 'cdktf-extensions', 
-    packageName: '@gplassard/cdktf-extensions',
+  name: 'cdktf-extensions',
+  packageName: '@gplassard/cdktf-extensions',
+  releaseRank: 2,
+  devDeps: ['cdktf', 'constructs'],
+  peerDeps: ['cdktf', 'constructs'],
 });
 project.synth();
