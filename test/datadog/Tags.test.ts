b8bb5b315b5beb8e27d7e7ac92667b7207fdaf2e
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2023-09-04 00:07:21 +0200

    feat(to-dd-tags): add toDatadogTags function (#2)

---
+++ test/datadog/Tags.test.ts
@@ -0,0 +1,24 @@
+import { toDatadogTags } from '../../src';
+
+describe('Tags.toDatadogTags', () => {
+  it('should format the tags correctly for datadog', () => {
+    const result = toDatadogTags({
+      app: 'test-app',
+      env: 'test-env',
+      repo: 'test-repo',
+      scope: 'test-scope',
+      team: 'test-team',
+      infraAsCode: 'test-infraAsCode' as any,
+      bonus: 'test-bonus',
+    });
+    expect(result).toEqual([
+      'app:test-app',
+      'env:test-env',
+      'repo:test-repo',
+      'scope:test-scope',
+      'team:test-team',
+      'infraAsCode:test-infraAsCode',
+      'bonus:test-bonus',
+    ]);
+  });
+});
